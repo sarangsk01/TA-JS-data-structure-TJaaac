0a22cb87cc1d91da464f1ab2aa0d202d9522a481
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-03-02 16:24:28 +0530

    New commit

--- block-BJaacf/code/index.js
+++ block-BJaacf/code/index.js
@@ -2,35 +2,56 @@
 let numbers = [25, 45, 78, 95, 65];
 
 // 2. Calculate the sum of array items and print it to the console using console.log()
-console.log(`The sum of array items is ${numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]}`);
+let sum = 0;
+for(let number of numbers){
+    sum += number;
+}
+console.log(sum);
 
 // 3. Calculate the average of array items and print it to the console using console.log()
-console.log(`The average of array items is ${(numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4])/5}`);
+console.log(sum / numbers.length);
 // 4. Find the highest number in the array and print it to the console using console.log()
-console.log(`The hughest number  of array items is ${numbers[0]>numbers[1]>numbers[2]>numbers[3]>numbers[4]}`);
-// 5. Find the lowest number in the array and print it to the console using console.log()
-
-// 6. Find the even numbers in the array and print them to the console using console.log()
-let odd = [], even = [];
+let highestNumber = numbers[0];
 for(let number of numbers){
-    if(number % 2 === 0){
-        even.push(number);
-    }else{
-        odd.push(number);
+    if(number > highestNumber){
+        highestNumber = number;
     }
 }
-console.log(`The even numbers in the array is ${even.push(number)}`);
+console.log(highestNumber);
+// 5. Find the lowest number in the array and print it to the console using console.log()
+let lowestNumber = numbers[0];
+for(let number of numbers){
+    if(number < lowestNumber){
+        lowestNumber = number;
+    }
+}
+console.log(lowestNumber);
+// 6. Find the even numbers in the array and print them to the console using console.log()
+for(let number of numbers){
+    if(number % 2 == 0){
+        console.log(number);
+    }
+}
 // 7. Find the odd numbers in the array and print them to the console using console.log()
 for(let number of numbers){
     if(number % 2 !== 0){
-        odd.push(number);
-    }else{
-        even.push(number);
+        console.log(number, "odd");
     }
 }
-console.log(`The odd numbers in the array is ${odd.push(number)}`);
 // 8. Find the numbers in the array that is divisible by 5 and print them to the console using console.log()
-
+for(let number of numbers){
+    if(number % 5 == 0){
+        console.log(number, "divisible by 5");
+    }
+}
 // 9. Log all the element of the array one by one
+for(let number of numbers){
+        console.log(number, "each value");
+    }
 
 // 10. Find all the number in the array that is divisible by 3
+for(let number of numbers){
+    if(number % 3 == 0){
+        console.log(number, "divisible by 3");
+    }
+}
